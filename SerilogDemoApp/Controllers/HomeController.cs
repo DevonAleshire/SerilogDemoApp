4054fb2edd0ae3644d3252c27da4ccd7e9e18dfc
Devon Aleshire <devoale@gmail.com>
2019-06-13 22:03:15 -0400

    Add project files.

---
+++ SerilogDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SerilogDemoApp.Models;
+
+namespace SerilogDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var blog = new Blog
+            //{
+            //    Url = "my url",
+            //};
+
+            //var post = new Post
+            //{
+            //    Title = "Title",
+            //    Content = "Content",
+            //};
+
+            //blog.Posts.Add(post);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SerilogDemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using System.Diagnostics;
+
+namespace SerilogDemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Serilog.Debugging.SelfLog.Enable(msg =>
+            {
+                Debug.Print(msg);
+                Debugger.Break();
+            });
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog((context, config) =>
+                {
+                    config.ReadFrom.Configuration(context.Configuration);
+                })
+                .UseStartup<Startup>();
+    }
+}
